bbb360d8444118fd81e56d55e52e734066cbcce9
SaraGrabowski <sgrabowski3149@stu.d214.org>
2023-04-04 14:27:50 -0500

    Message1

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer: Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                
+            } label: {
+                Text("Multiply")
+            }
+            Text("\(answer)")
         }
     }
 }
