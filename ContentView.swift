462d8db236320e750b8553744104226fe127e94b
Tanya Cervantes <tcervantes3491@stu.d214.org>
2023-04-04 14:30:29 -0500

    2

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var numberOne = 0
-    @State var numberTwo = 0
+    @State var numberOne: Int
+    @State var numberTwo: Int
     
     var body: some View {
-        
         TextField("Enter Number", value: $numberOne, format: .number)
         
         TextField("Enter Number", value: $numberTwo, format: .number)
